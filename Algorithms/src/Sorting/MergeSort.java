405083189483fc825288e5d7126f11fe0f07b9cc
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2017-02-14 22:22:50 +0000

    Cleaner merge sort
    CLRS 2.3

--- Algorithms/src/Sorting/MergeSort.java
+++ Algorithms/src/Sorting/MergeSort.java
@@ -3,55 +3,43 @@ package Sorting;
 import java.util.Arrays;
 
 public class MergeSort {
-	public static int[] combine(int[] a, int[] b) {
-		int[] output = new int[a.length + b.length];
-		int i = 0;
-		int posA = 0;
-		int posB = 0;
-		while(posA != a.length  && posB != b.length) {
-			if(a[posA] < b[posB]) {
-				output[i] = a[posA];
-				posA++;
-			} else {
-				output[i] = b[posB];
-				posB++;
-			}
-			i++;
+	public static int[] mergeSort(int[] a, int p, int r) {
+		if(p < r) {
+			int q = (p + r) / 2;
+			mergeSort(a, p, q);
+			mergeSort(a, q + 1, r);
+			merge(a, p, q, r);
 		}
-		while(posA != a.length) {
-			output[i] = a[posA];
-			posA++;
-			i++;
-		}
-		while(posB != b.length) {
-			output[i] = b[posB];
-			posB++;
-			i++;
-		}
-		return output;
+		return a;
 	}
 	
-	public static int[] mergeSort(int[] a) {
-		if(a.length == 0 || a.length == 1) {
-			return a;
-		} else if(a.length == 2) {
-			if(a[0] > a[1]) {
-				int[] output = {a[1], a[0]};
-				return output;
+	public static int[] merge(int[] a, int p, int q, int r) {
+		int posP = p;
+		int posQ = q+1;
+		int[] nova = new int[r - p + 1];
+		for(int i = 0; i < (r - p + 1); i++) {
+			if(posQ > r || a[posP] < a[posQ]) {
+				nova[i] = a[posP];
+				posP++;
 			} else {
-				return a;
+				nova[i] = a[posQ];
+				posQ++;
 			}
-		} else {
-			int[] part1 = new int[a.length/2];
-			int[] part2 = new int[a.length - part1.length];
-			System.arraycopy(a, 0, part1, 0, part1.length);
-			System.arraycopy(a, part1.length, part2, 0, part2.length);
-			return combine(mergeSort(part1), mergeSort(part2));
 		}
+		for(int i = 0; i < (r - p + 1); i++) {
+			a[p + i] = nova[i];
+		}
+		return a;
+	}
+			
+	public static int[] mergeSort(int[] a) {
+		return mergeSort(a, 0, a.length);
 	}
 	
 	public static void main(String[] args) {
 		int[] b = {3, 9, 8, 3, 5, 2, 1};
-		System.out.println(Arrays.toString(mergeSort(b)));
+		int[] c = {3, 9, 2, 8, 5, 2, 1};
+		int[] d = {};
+		System.out.println(Arrays.toString(mergeSort(b, 0, 6)));
 	}
 }
