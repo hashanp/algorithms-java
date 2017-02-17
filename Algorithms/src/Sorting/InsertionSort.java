9f034a65f1080f35491d63ce144bcba375bff9fc
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2017-02-17 18:23:08 +0000

    Updated insertion sort so it occurs in-place

--- Algorithms/src/Sorting/InsertionSort.java
+++ Algorithms/src/Sorting/InsertionSort.java
@@ -3,23 +3,21 @@ package Sorting;
 import java.util.Arrays;
 
 public class InsertionSort {
-	public static int[] insertionSort(int[] input) {
-		int[] output = new int[input.length];
-		for(int i = 0; i < input.length; i++) {
-			output[i] = input[i];
+	public static void insertionSort(int[] input) {
+		for(int i = 1; i < input.length; i++) {
+			int key = input[i];
 			int j = i - 1;
-			while(j >= 0 && output[j] > output[j+1]) {
-				int c = output[j+1];
-				output[j+1] = output[j];
-				output[j] = c;
+			while(j >= 0 && input[j] > key) {
+				input[j + 1] = input[j];
 				j--;
 			}
+			input[j + 1] = key;
 		}
-		return output;
 	}
 	
 	public static void main(String[] args) {
 		int b[] = {4, 6, 3, 5, 2, 9, 11, 9};
-		System.out.println(Arrays.toString(insertionSort(b)));
+		insertionSort(b);
+		System.out.println(Arrays.toString(b));
 	}
 }
