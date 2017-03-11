b1a970c7df4a81bc44c601596c7f8b7945d88482
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2017-03-11 16:33:14 +0000

    Improves my implementation of quicksort to avoid a Θ(n) worst-case space complexity.

--- Algorithms/src/Sorting/QuickSort.java
+++ Algorithms/src/Sorting/QuickSort.java
@@ -8,7 +8,7 @@ public class QuickSort {
 		// lastSmall refers to the last index that points to a value less 
 		// than the pivot where lastSmall <= i
 		int lastSmall = p - 1;
-		for(int i = 0; i < r; i++) {
+		for(int i = p; i < r; i++) {
 			if(arr[i] <= pivot) {
 				lastSmall++;
 				// Swap arr[lastSmall] and arr[i]
@@ -24,21 +24,70 @@ public class QuickSort {
 		// Return the place of pivot
 		return lastSmall+1;
 	}
-	
+
+	/*
+	 * This version of quickSort is only good in the average case,
+	 * whenever the input size is either close to being sorted, or 
+	 * close to being sorted in reverse order, and the input size 
+	 * is large, a stack overflow error will occur. This is because 
+	 * the pivot will be consistently either the biggest or smallest 
+	 * item. This means there is Θ(n) worst case space complexity, due to 
+	 * Θ(n) stack frames. For large input sizes, this leads to stack
+	 * overflow errors.
+	 * 
+	 * In the average case there will be only Θ(lg n) stack frames, 
+	 * hence no need to worry.
+	 */
 	public static void quickSort(int[] arr, int p, int r) {
 		if(p < r) {
-			int pivot = partition(arr, 0, r);
-			quickSort(arr, 0, pivot-1);
-			quickSort(arr, pivot+1, r);
+			int pivot = partition(arr, p, r);
+			quickSort(arr, p, pivot-1);
+			quickSort(arr, pivot+1, r);			
 		}
 	}
 	
 	public static void quickSort(int[] arr) {
-		quickSort(arr, 0, arr.length-1);
+		quickSort(arr, 0, arr.length - 1);
+	}
+	
+	/*
+	 * This is a better implementation of quickSort, which I use
+	 * in my benchmarking class. I have to use iteration, since
+	 * Java does not (alas!) perform tail-call optimisation. This
+	 * algorithm, finds which subarray, that has resulted from the 
+	 * partition, is smaller. It recursively computes quickSort
+	 * on this subarray. It then performs quickSort on the larger 
+	 * subarray, but it uses iteration so no new stack frames are
+	 * created.
+	 * 
+	 * In a programming language that supports tail-call optimisation,
+	 * it would be possible to avoid the use of iteration, as the final
+	 * recursive invocation of betterQuickSort by itself, would be 
+	 * automatically converted to use iteration.
+	 */
+	public static void betterQuickSort(int[] arr, int p, int r) {
+		while(p < r) {
+			int pivot = partition(arr, p, r);
+			if(pivot - p < r - pivot) {
+				quickSort(arr, p, pivot - 1);
+				p = pivot + 1;
+			} else {
+				quickSort(arr, pivot + 1, r);
+				r = pivot - 1;
+			}
+		}
+	}
+	
+	public static void betterQuickSort(int[] arr) {
+		betterQuickSort(arr, 0, arr.length - 1);
+	}
+	
+	public static void randomisedQuickSort(int[] arr) {
+		quickSort(Randomise.randomise(arr));
 	}
 	
 	public static void main(String[] args) {
-		int[] a = {2, 8, 7, 1, 3, 5, 6, 4};
+		int[] a = {2, 8, 7, 35, 34, 1, 3, 5, 6, 4};
 		quickSort(a);
 		System.out.println(Arrays.toString(a));
 	}
