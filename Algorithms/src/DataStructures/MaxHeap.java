5d5ec2c267c813a49bd10a1a6a8df8b6a9d3130a
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2018-04-19 00:21:54 +0100

    Fixes style issues for max heap implementation

--- Algorithms/src/DataStructures/MaxHeap.java
+++ Algorithms/src/DataStructures/MaxHeap.java
@@ -10,11 +10,11 @@ public class MaxHeap {
   public void add(int item) {
     int index = data.size();
     data.add(item);
-    while(index != 0) {
+    while (index != 0) {
       int oldIndex = index;
-      index = (index-1)/2;
+      index = (index - 1) / 2;
       int k = data.get(index);
-      if(k < item) {
+      if (k < item) {
         data.set(index, item);
         data.set(oldIndex, k);
       } else {
@@ -25,31 +25,31 @@ public class MaxHeap {
 
   public int remove() {
     int c = data.get(0);
-    int last = data.get(data.size()-1);
+    int last = data.get(data.size() - 1);
     data.set(0, last);
-    data.remove(data.size()-1);
+    data.remove(data.size() - 1);
     int index = 0;
-    while(true) {
-      if((2*index)+2 > data.size()) {
+    while (true) {
+      if ((2 * index) + 2 > data.size()) {
         break;
-      } else if((2*index)+2 == data.size()) {
-        int leftChild = data.get((2*index)+1);
-        if(last < leftChild) {
-          data.set((2*index)+1, last);
+      } else if ((2 * index) + 2 == data.size()) {
+        int leftChild = data.get((2 * index) + 1);
+        if (last < leftChild) {
+          data.set((2 * index) + 1, last);
           data.set(index, leftChild);
         }
         break;
       } else {
-        int leftChild = data.get((2*index)+1);
-        int rightChild = data.get((2*index)+2);
-        if(leftChild > rightChild && last < leftChild) {
-          data.set((2*index)+1, last);
+        int leftChild = data.get((2 * index) + 1);
+        int rightChild = data.get((2 * index) + 2);
+        if (leftChild > rightChild && last < leftChild) {
+          data.set((2 * index) + 1, last);
           data.set(index, leftChild);
-          index = (2*index)+1;
+          index = (2 * index) + 1;
         } else if(last < rightChild) {
-          data.set((2*index)+2, last);
+          data.set((2 * index) + 2, last);
           data.set(index, rightChild);
-          index = (2*index)+2;
+          index = (2 * index) + 2;
         } else {
           break;
         }
