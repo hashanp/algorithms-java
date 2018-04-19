f718da6efd900e5a317336c73a51a2ad4536f1f7
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2018-04-19 12:48:55 +0100

    Fixes style issues in binary search implementation

--- Algorithms/src/Searching/BinarySearch.java
+++ Algorithms/src/Searching/BinarySearch.java
@@ -8,13 +8,13 @@ public class BinarySearch {
 
   public static int binarySearch(int[] arr, int term) {
     int min = 0;
-    int max = arr.length-1;
-    while(min <= max) {
-      int pos = (min + max) /2;
-      if(arr[pos] < term) {
-        min = pos+1;
-      } else if(arr[pos] > term) {
-        max = pos-1;
+    int max = arr.length - 1;
+    while (min <= max) {
+      int pos = (min + max) / 2;
+      if (arr[pos] < term) {
+        min = pos + 1;
+      } else if (arr[pos] > term) {
+        max = pos - 1;
       } else {
         return pos;
       }
