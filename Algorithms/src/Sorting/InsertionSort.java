ce3839925078d33a460eb4278cd2352e49d22d44
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2018-04-19 14:56:21 +0100

    Changes indentation for InsertionSort.java

--- Algorithms/src/Sorting/InsertionSort.java
+++ Algorithms/src/Sorting/InsertionSort.java
@@ -3,21 +3,21 @@ package Sorting;
 import java.util.Arrays;
 
 public class InsertionSort {
-	public static void insertionSort(int[] input) {
-		for(int i = 1; i < input.length; i++) {
-			int key = input[i];
-			int j = i - 1;
-			while(j >= 0 && input[j] > key) {
-				input[j + 1] = input[j];
-				j--;
-			}
-			input[j + 1] = key;
-		}
-	}
-	
-	public static void main(String[] args) {
-		int b[] = {4, 6, 3, 5, 2, 9, 11, 9};
-		insertionSort(b);
-		System.out.println(Arrays.toString(b));
-	}
+  public static void insertionSort(int[] input) {
+    for(int i = 1; i < input.length; i++) {
+      int key = input[i];
+      int j = i - 1;
+      while(j >= 0 && input[j] > key) {
+        input[j + 1] = input[j];
+        j--;
+      }
+      input[j + 1] = key;
+    }
+  }
+
+  public static void main(String[] args) {
+    int b[] = {4, 6, 3, 5, 2, 9, 11, 9};
+    insertionSort(b);
+    System.out.println(Arrays.toString(b));
+  }
 }
