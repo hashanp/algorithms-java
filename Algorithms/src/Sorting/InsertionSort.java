9b78c541402daa5b32e56650210af861b44131e5
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2018-04-19 14:57:02 +0100

    Fixes styles issues in insertion sort implementation

--- Algorithms/src/Sorting/InsertionSort.java
+++ Algorithms/src/Sorting/InsertionSort.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class InsertionSort {
   public static void insertionSort(int[] input) {
-    for(int i = 1; i < input.length; i++) {
+    for (int i = 1; i < input.length; i++) {
       int key = input[i];
       int j = i - 1;
-      while(j >= 0 && input[j] > key) {
+      while (j >= 0 && input[j] > key) {
         input[j + 1] = input[j];
         j--;
       }
